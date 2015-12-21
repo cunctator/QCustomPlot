106c28061020f6847f99aded1edff2b2dbd3adc8
Emanuel Eichhammer <support@qcustomplot.com>
2015-12-22 00:57:49 +0100

    - added qmake550 to test scripts
    (cherry picked from commit 94cbf907ce72f312a1a309548a394228d8012c06)

--- tests/auto/run-autotest.py
+++ tests/auto/run-autotest.py
@@ -33,7 +33,7 @@ def runTest():
   os.remove("./Makefile")
     
 # main test loop:
-qmakeVersions = ["qmake464", "qmake474", "qmake486", "qmake501", "qmake511", "qmake520", "qmake532", "qmake540"]
+qmakeVersions = ["qmake464", "qmake474", "qmake486", "qmake501", "qmake511", "qmake520", "qmake532", "qmake540", "qmake550"]
 if (config.qt > 0):
   qmakeVersions = ["qmake"+str(config.qt)]
 

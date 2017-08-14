9a3ac74485527c920b0837e69cf699fc9b05850b
Emanuel Eichhammer <support@qcustomplot.com>
2017-08-14 23:12:22 +0200

    - doc: made run-doxygen script less verbose (tools only print STDERR) - doc: postprocessing python scripts now separate standard output from error output

--- run-doxygen.sh
+++ run-doxygen.sh
@@ -4,17 +4,18 @@ rm -r documentation/html
 mkdir documentation/html
 
 # let doxygen generate qhp index and html hierarchy from source files:
-doxygen
+echo Running doxygen...
+doxygen 1>/dev/null
 
 # do post-processing on documentation images and html hierarchy:
 echo Post-processing documentation images...
-./documentation/postprocess-doc-images.py
+./documentation/postprocess-doc-images.py 1>/dev/null
 echo Post-processing documentation html...
-./documentation/postprocess-doc-html.py
+./documentation/postprocess-doc-html.py 1>/dev/null
 
 # generate qhc file from qhp index and html for QtAssistant integration:
 echo Building qhc...
-qhelpgenerator documentation/html/index.qhp -o documentation/qthelp/qcustomplot.qch
+qhelpgenerator documentation/html/index.qhp -o documentation/qthelp/qcustomplot.qch 1>/dev/null
 
 # cleanup html hierarchy:
 rm documentation/html/index.qhp
